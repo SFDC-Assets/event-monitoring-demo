ebb66e7530a96284f708d1a67f1883a18c5d263e
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-05-24 08:38:20 -0700

    orgInit

--- orgInit.sh
+++ orgInit.sh
@@ -1,1 +1,7 @@
-sfdx force:package:instal 04t1t000003DLALAA4
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix event-monitoring -o work.shop
+sfdx force:source:push
+sfdx shane:user:password:set -p sfdx1234 -g User -l User
+sfdx force:org:open
+
+//sfdx force:package:install -p 04t1t000003DLALAA4 -w 10
+// sfdx force:package:install -p 04t5w000005hfrtAAA -w 10
