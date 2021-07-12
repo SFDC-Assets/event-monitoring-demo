2db5cdbc5e564d721a5fe6560ce175790b4f7b3d
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-07-11 21:47:46 -0700

    iteration 1.1

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix event-monitoring -o work.shop | cut -b 98-
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix event-monitoring -o work.shop
 sfdx shane:tsp:username:update
 sfdx force:package:install -p 04t1t000003DLALAA4 -w 10
 sfdx force:source:push
