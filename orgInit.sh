2a13482f33293e1e3d9f399d1cf93c462e48a8d0
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-07-11 21:47:05 -0700

    iteration 1

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,9 @@
-UORGNAME=$(sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix event-monitoring -o work.shop | cut -b 98-)
-sed -i '' -e 's/PLACEHOLDER/'$UORGNAME'/g' ./force-app/main/default/transactionSecurityPolicies/*
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix event-monitoring -o work.shop | cut -b 98-
+sfdx shane:tsp:username:update
 sfdx force:package:install -p 04t1t000003DLALAA4 -w 10
 sfdx force:source:push
-sfdx shane:user:password:set -p sfdx1234 -g User -l User
+sfdx force:user:permset:assign -n Event_Monitoring_Access
+sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:org:open
 
 // sfdx force:package:install -p 04t5w000005hfrtAAA -w 10
