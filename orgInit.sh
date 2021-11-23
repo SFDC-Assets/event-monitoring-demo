9c36061722a1d2cdbe681564bb0265cc51f27509
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2021-11-23 12:58:07 -0800

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix event-monitoring -o work.shop
 sfdx shane:tsp:username:update
-sfdx force:package:install -p 04t1t000003DLALAA4 -w 10
+sfdx force:package:install -p 04t1t000003Po3QAAS -w 10
 sfdx force:source:push
 sfdx force:user:permset:assign -n Event_Monitoring_Access
 sfdx shane:user:password:set -p salesforce1 -g User -l User
