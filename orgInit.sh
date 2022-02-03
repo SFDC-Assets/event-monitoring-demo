7b113cfef741a5a4b6de48cac7325bb92018c3ed
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2022-02-03 17:09:44 -0500

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix event-monitoring -o work.shop
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --userprefix event-monitoring -o work.shop
 sfdx shane:tsp:username:update
 sfdx force:package:install -p 04t1t000003Po3QAAS -w 10
 sfdx force:source:push
