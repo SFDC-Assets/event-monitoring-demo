bae2a02be84f944029f191833b0e47174e1e13fa
zhou059 <zhou059@gmail.com>
2024-10-02 11:40:45 -0700

    latestCLI

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,8 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --userprefix event-monitoring -o work.shop
-sfdx shane:tsp:username:update
-sfdx force:package:install -p 04t1t000003Po3QAAS -w 10
-sfdx force:source:push
-sfdx force:user:permset:assign -n Event_Monitoring_Access
-sfdx shane:user:password:set -p salesforce1 -g User -l User
+sf demoutil org create scratch -f config/project-scratch-def.json -d 30 -s --userprefix event-monitoring -o work.shop
+sf demoutil tsp update
+sf package install -p 04t1t000003Po3QAAS -w 10
+sf project deploy start
+sf org assign permset -n Event_Monitoring_Access
+sf demoutil user password set -g User -l User -p salesforce1
 sf org create user -a other-user
-sfdx force:org:open
+sf org open
