dd42ba477d24d39c0afbeba505d3863cf785bcf0
zhou059 <zhou059@gmail.com>
2024-10-02 15:51:35 -0700

    latestCLI

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sf demoutil org create scratch -f config/project-scratch-def.json -d 30 -s -p event-monitoring -e work.shop
+sf demoutil org create scratch -p event -e monitoring.demo -d 20 -w 10 -f config/project-scratch-def.json -s
 sf demoutil tsp update
 sf package install -p 04t1t000003Po3QAAS -w 10
 sf project deploy start
