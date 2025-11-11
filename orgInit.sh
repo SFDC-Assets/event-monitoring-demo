8f88fa0ba6cbcb8cc2805fb7c2cca58862db29c1
Jonathan Zhou <jonathan.zhou@jonathan-ltmcf4y.internal.salesforce.com>
2025-11-10 23:10:01 -0800

    cleanup

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sf demoutil org create scratch -p event -e monitoring.demo -d 20 -w 10 -f config/project-scratch-def.json -s
 sf demoutil tsp update
-sf package install -p 04t1t000003Po3QAAS -w 10
+sf package install -p 04tJ5000000gQFxIAM -w 10
 sf project deploy start
 sf org assign permset -n Event_Monitoring_Access
 sf demoutil user password set -g User -l User -p salesforce1
